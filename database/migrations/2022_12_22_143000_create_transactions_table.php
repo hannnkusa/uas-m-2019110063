6d33997ffbfa79eb5830ee54f0788bafeb770fed
Handy Kusuma <handyk1709@gmail.com>
2022-12-23 00:02:57 +0700

    created model, migration, controller, and lang

---
+++ database/migrations/2022_12_22_143000_create_transactions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->string('kategori', 50);
+            $table->double('nominal');
+            $table->string('tujuan');
+            $table->char('account_id', 16);
+            $table->foreign('account_id')->references('id')->on('accounts')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
